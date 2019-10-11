33480b3d3a6e28b50720b245fd9e86871923ed9c
jahn <ioannis.kiwe@gmail.com>
2019-10-11 14:10:12 +0200

    test/routes/healthcheck.test.js: Added tests for worker-widget /healthcheck route

--- test/routes/healthcheck.test.js
+++ test/routes/healthcheck.test.js
@@ -13,7 +13,7 @@ const attachHealthcheckRouter = () => {
     return app;
 };
 
-describe("when testing the healthcheck route (integration-like tests)", () => {
+describe("when testing the healthcheck routes (integration-like tests)", () => {
     let app;
     beforeEach(() => {
         app = attachHealthcheckRouter();
@@ -22,54 +22,123 @@ describe("when testing the healthcheck route (integration-like tests)", () => {
         process.env.KAFKA_CONNECT_TARGET_WORKER_IDS = undefined;
     });
 
-    describe("and there is a failure in the targeted worker", () => {
-        const connector = "test-connector-0";
-        let response;
-        beforeEach(async () => {
-            nock("http://localhost:8083")
-                .get(`/connectors/${connector}/status`)
-                .reply(200, {
-                    status: 200,
-                    tasks: [
-                        { id: 0, worker_id: testWorkerId, state: "FAILED" },
-                        { id: 1, worker_id: testWorkerId, state: "RUNNING" },
-                    ],
+    describe("when testing the worker-wide /healthcheck route", () => {
+        const connectors = ["test-connector-0", "test-connector-1"];
+        describe("and there is a failure in the targeted worker", () => {
+            let response;
+            beforeEach(async () => {
+                nock("http://localhost:8083")
+                    .get(`/connectors`)
+                    .reply(200, connectors);
+
+                connectors.forEach((connector) => {
+                    nock("http://localhost:8083")
+                        .get(`/connectors/${connector}/status`)
+                        .reply(200, {
+                            status: 200,
+                            tasks: [
+                                { id: 0, worker_id: testWorkerId, state: "FAILED" },
+                                { id: 1, worker_id: testWorkerId, state: "RUNNING" },
+                            ],
+                        });
                 });
-            response = await request(app).get(`/healthcheck/${connector}`);
+                response = await request(app).get(`/healthcheck`);
+            });
+
+            it("should return a 503 status", () => {
+                expect(response.status).to.equal(503);
+            });
+
+            const expectedResponseBody = [
+                { workerId: testWorkerId, taskId: 0 },
+                { workerId: testWorkerId, taskId: 0 },
+            ];
+            it("should return a body with the expected failure message", () => {
+                expect(response.body).to.equal(`Failures: ${JSON.stringify(expectedResponseBody)}`);
+            });
         });
 
-        it("should return a 503 status", () => {
-            expect(response.status).to.equal(503);
-        });
+        describe("and there are no failing tasks in the targeted worker", () => {
+            let response;
+            beforeEach(async () => {
+                nock("http://localhost:8083")
+                    .get(`/connectors`)
+                    .reply(200, connectors);
 
-        it("should return a body with the expected failure message", () => {
-            expect(response.body).to.equal(
-                `Failures: ${JSON.stringify([{ workerId: testWorkerId, taskId: 0 }])}`
-            );
+                connectors.forEach((connector) => {
+                    nock("http://localhost:8083")
+                        .get(`/connectors/${connector}/status`)
+                        .reply(200, {
+                            status: 200,
+                            tasks: [
+                                { id: 0, worker_id: "another-one:3737", state: "FAILED" },
+                                { id: 1, worker_id: testWorkerId, state: "RUNNING" },
+                            ],
+                        });
+                });
+                response = await request(app).get(`/healthcheck`);
+            });
+
+            it("should return a 200 status", () => {
+                expect(response.status).to.equal(200);
+            });
+
+            it("should return a body with the 'OK' message", () => {
+                expect(response.body).to.equal("OK");
+            });
         });
     });
-    describe("and there is no failure in the targeted worker", () => {
-        const connector = "test-connector-1";
-        let response;
-        beforeEach(async () => {
-            nock("http://localhost:8083")
-                .get(`/connectors/${connector}/status`)
-                .reply(200, {
-                    status: 200,
-                    tasks: [
-                        { id: 0, worker_id: "another-one:3737", state: "FAILED" },
-                        { id: 1, worker_id: testWorkerId, state: "RUNNING" },
-                    ],
-                });
-            response = await request(app).get(`/healthcheck/${connector}`);
-        });
 
-        it("should return a 200 status", () => {
-            expect(response.status).to.equal(200);
-        });
+    describe("when testing the connector-specific /healthcheck/:connector route", () => {
+        describe("and there is a failure in tasksof the targeted connector in the argeted worker", () => {
+            const connector = "test-connector-0";
+            let response;
+            beforeEach(async () => {
+                nock("http://localhost:8083")
+                    .get(`/connectors/${connector}/status`)
+                    .reply(200, {
+                        status: 200,
+                        tasks: [
+                            { id: 0, worker_id: testWorkerId, state: "FAILED" },
+                            { id: 1, worker_id: testWorkerId, state: "RUNNING" },
+                        ],
+                    });
+                response = await request(app).get(`/healthcheck/${connector}`);
+            });
 
-        it("should return a body with the 'OK' message", () => {
-            expect(response.body).to.equal("OK");
+            it("should return a 503 status", () => {
+                expect(response.status).to.equal(503);
+            });
+
+            it("should return a body with the expected failure message", () => {
+                expect(response.body).to.equal(
+                    `Failures: ${JSON.stringify([{ workerId: testWorkerId, taskId: 0 }])}`
+                );
+            });
+        });
+        describe("and there is no failure in the targeted worker", () => {
+            const connector = "test-connector-1";
+            let response;
+            beforeEach(async () => {
+                nock("http://localhost:8083")
+                    .get(`/connectors/${connector}/status`)
+                    .reply(200, {
+                        status: 200,
+                        tasks: [
+                            { id: 0, worker_id: "another-one:3737", state: "FAILED" },
+                            { id: 1, worker_id: testWorkerId, state: "RUNNING" },
+                        ],
+                    });
+                response = await request(app).get(`/healthcheck/${connector}`);
+            });
+
+            it("should return a 200 status", () => {
+                expect(response.status).to.equal(200);
+            });
+
+            it("should return a body with the 'OK' message", () => {
+                expect(response.body).to.equal("OK");
+            });
         });
     });
 });
